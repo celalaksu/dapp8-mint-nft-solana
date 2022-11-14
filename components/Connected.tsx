4445841f210edda95dabf2f605382d68976e1d95
Celal AKSU <celalaksu@gmail.com>
2022-11-14 19:59:09 +0300

    first commit

--- components/Connected.tsx
+++ components/Connected.tsx
@@ -23,7 +23,6 @@ import {
   CandyMachine,
 } from "@metaplex-foundation/js";
 import { useRouter } from "next/router";
-import styles from "../styles/custom.module.css";
 
 const Connected: FC = () => {
   const { connection } = useConnection();
@@ -35,98 +34,23 @@ const Connected: FC = () => {
     return Metaplex.make(connection).use(walletAdapterIdentity(walletAdapter));
   }, [connection, walletAdapter]);
 
-  const [getNftData, setNftData] = useState([]);
-  const [page, setPage] = useState(1);
-
-  const [pageItems, setPageItems] = useState<any>();
-  // useEffect(() => {
-  //   if (!metaplex) return
-
-  //   metaplex
-  //     .candyMachines()
-  //     .findByAddress({
-  //       address: new PublicKey("ELgav1kTkmmu58rq33SfVYCD3YJGCU5TobwQRivtukie"),
-  //     })
-  //     .run()
-  //     .then((candyMachine) => {
-  //       console.log(candyMachine)
-  //       setCandyMachine(candyMachine)
-  //     })
-  //     .catch((error) => {
-  //       alert(error)
-  //     })
-  // }, [metaplex])
-
-  console.log(candyMachine?.items.length);
-
-  const fetchCandyMachine = async () => {
-    setPage(1);
-    // fetch candymachine data
-    try {
-      const candyMachine = await metaplex
-        .candyMachines()
-        .findByAddress({
-          address: new PublicKey(
-            "ELgav1kTkmmu58rq33SfVYCD3YJGCU5TobwQRivtukie"
-          ),
-        })
-        .run();
-
-      // let nftData = []
-      // for (let i=0; i < candyMachine.items.length; i++){
-      //     let fetchResult = await fetch(candyMachine.items[i].uri)
-      //     let json = await fetchResult.json()
-      //     nftData.push(json)
-      // }
-
-      //console.log("FALAN FİLAN İNTER MİLAN", nftData)
-      setCandyMachine(candyMachine);
-      //setNftData(nftData)
-    } catch (e) {
-      alert("Please submit a valid CMv2 address.");
-    }
-  };
-
-  const getPage = async (page: number, perPage: number) => {
-    const pageItems = candyMachine?.items.slice(
-      (page - 1) * perPage,
-      page * perPage
-    );
-
-    let nftData = [];
-    for (let i = 0; i < pageItems.length; i++) {
-      let fetchResult = await fetch(pageItems[i].uri);
-      let json = await fetchResult.json();
-      nftData.push(json);
-    }
-    setPageItems(nftData);
-  };
-
-  // previous page
-  const prev = async () => {
-    if (page - 1 < 1) {
-      setPage(1);
-    } else {
-      setPage(page - 1);
-    }
-  };
-
-  // next page
-  const next = async () => {
-    setPage(page + 1);
-  };
-
   useEffect(() => {
-    fetchCandyMachine();
-  }, []);
+    if (!metaplex) return;
 
-  // fetch metadata for NFTs when page or candy machine changes
-  useEffect(() => {
-    if (!candyMachine) {
-      return;
-    }
-    getPage(page, 9);
-  }, [candyMachine, page]);
+    metaplex
+      .candyMachines()
+      .findByAddress({
+        address: new PublicKey("ELgav1kTkmmu58rq33SfVYCD3YJGCU5TobwQRivtukie"),
+      })
+      .run()
+      .then((candyMachine) => {
+        console.log(candyMachine);
+        setCandyMachine(candyMachine);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, [metaplex]);
 
   const router = useRouter();
 
@@ -174,28 +98,13 @@ const Connected: FC = () => {
           </Text>
         </VStack>
       </Container>
-
-      {pageItems && (
-        <div>
-          <div className={styles.gridNFT}>
-            {pageItems.map((nft) => (
-              <div key={nft.name}>
-                <ul>{nft.name}</ul>
-                <Image src={nft.image} height="100px" width="100px" alt="" />
-              </div>
-            ))}
-          </div>
-          <div>
-            <Button colorScheme="teal" size="sm" onClick={prev}>
-              Prev
-            </Button>
-            <Button colorScheme="teal" size="sm" onClick={next}>
-              Next
-            </Button>
-          </div>
-        </div>
-      )}
-
+      <HStack spacing={10}>
+        <Image src="avatar1.png" height="150px" alt="" />
+        <Image src="avatar2.png" height="150px" alt="" />
+        <Image src="avatar3.png" height="150px" alt="" />
+        <Image src="avatar4.png" height="150px" alt="" />
+        <Image src="avatar5.png" height="150px" alt="" />
+      </HStack>
       <Button
         bgColor="accent"
         color="white"
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-
 import type { NextPage } from "next";
 import Disconnected from "../components/Disconnected";
 import Connected from "../components/Connected";
